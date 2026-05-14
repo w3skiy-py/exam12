bd0499a083949e35c3c9995e03dcb71748ec3ae4
w3skiy-py <onewinterwork@gmail.com>
2026-05-14 11:58:09 +0300

    v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwerty@127.0.0.1:5432/examflask'
-app.config['SECRET_KEY'] = 'some_secret_key'
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
